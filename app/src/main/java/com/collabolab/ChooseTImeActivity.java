20b3eb1137594801ed97fdcc66b225a554eea2f9
SnoopyKim <snoopykim0@gmail.com>
2018-12-28 02:18:02 +0900

    change choose time button to recyclerview

--- app/src/main/java/com/collabolab/ChooseTImeActivity.java
+++ app/src/main/java/com/collabolab/ChooseTImeActivity.java
@@ -2,41 +2,32 @@ package com.collabolab;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class ChooseTImeActivity extends Activity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
 
-    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8;
+public class ChooseTImeActivity extends Activity {
+
+    List<Integer> time = new ArrayList<>();
+
+    RecyclerView rvTimeBlock;
+    LinearLayoutManager layoutManager;
+    TimeBlockAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_time);
-        init();
+
+        for (int i=0; i<24; i++) { time.add(i); }
+
+        rvTimeBlock = findViewById(R.id.rv_time);
+        layoutManager = new LinearLayoutManager(this);
+        rvTimeBlock.setLayoutManager(layoutManager);
+        adapter = new TimeBlockAdapter(time, this);
+        rvTimeBlock.setAdapter(adapter);
     }
 
-    public void init() {
-        btn1 = findViewById(R.id.btn_1);
-        btn1.setOnClickListener(this);
-        btn2 = findViewById(R.id.btn_2);
-        btn2.setOnClickListener(this);
-        btn3 = findViewById(R.id.btn_3);
-        btn3.setOnClickListener(this);
-        btn4 = findViewById(R.id.btn_4);
-        btn4.setOnClickListener(this);
-        btn5 = findViewById(R.id.btn_5);
-        btn5.setOnClickListener(this);
-        btn6 = findViewById(R.id.btn_6);
-        btn6.setOnClickListener(this);
-        btn7 = findViewById(R.id.btn_7);
-        btn7.setOnClickListener(this);
-        btn8 = findViewById(R.id.btn_8);
-        btn8.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        ((Button)view).setSelected(!(((Button)view).isSelected()));
-    }
 }
