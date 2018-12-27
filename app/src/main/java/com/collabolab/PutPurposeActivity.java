19658e4978e130918d12a01ec68a47e94ac52e93
SnoopyKim <snoopykim0@gmail.com>
2018-12-28 08:18:53 +0900

    set

--- app/src/main/java/com/collabolab/PutPurposeActivity.java
+++ app/src/main/java/com/collabolab/PutPurposeActivity.java
@@ -1,10 +1,14 @@
 package com.collabolab;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 public class PutPurposeActivity extends AppCompatActivity {
@@ -15,6 +19,36 @@ public class PutPurposeActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_put_purpose);
+
+        ImageButton ibMenu = findViewById(R.id.ib_menu);
+        ibMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(getApplicationContext(), view);
+                popup.getMenuInflater().inflate(R.menu.popup, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Intent intent;
+                        if (item.getTitle().equals("상세보기")) {
+                            intent = new Intent(getApplicationContext(), ShowActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else if (item.getTitle().equals("예약하기")) {
+                            intent = new Intent(getApplicationContext(), SetDateActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else if (item.getTitle().equals("내 정보")) {
+                            intent = new Intent(getApplicationContext(), MyInfoActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        return true;
+                    }
+                });
+                popup.show();//showing popup menu
+            }
+        });
 
         FloatingActionButton btn = findViewById(R.id.btn_next);
         btn.setOnClickListener(new View.OnClickListener() {
