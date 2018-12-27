0679f4ea6f93acd8c28de1e853af2fdadd65e749
SnoopyKim <snoopykim0@gmail.com>
2018-12-28 00:26:56 +0900

    add ChooseTimeActivity

--- app/src/main/java/com/collabolab/RoomReservationActivity.java
+++ app/src/main/java/com/collabolab/RoomReservationActivity.java
@@ -1,15 +1,16 @@
 package com.collabolab;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 
-public class RoomReservationActivity extends AppCompatActivity  {
+
+public class RoomReservationActivity extends AppCompatActivity {
 
 
     private ViewPager mViewPager;
@@ -24,9 +25,10 @@ public class RoomReservationActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_roomreservation);
         mViewPager = (ViewPager) findViewById(R.id.viewPager);
-
 
         mCardAdapter = new CardPagerAdapter();
         mCardAdapter.addCardItem(new CardItem(R.string.title_1, R.string.text_1));
@@ -42,6 +44,15 @@ public class RoomReservationActivity extends AppCompatActivity  {
         mViewPager.setAdapter(mCardAdapter);
         mViewPager.setPageTransformer(false, mFragmentCardShadowTransformer);
         mViewPager.setOffscreenPageLimit(3);
+
+        Button btnDone = findViewById(R.id.cardTypeBtn);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ChooseTImeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public static float dpToPixels(int dp, Context context) {
